97f06c0208d8ae3438df596966db7dd4e6ba5ad9
FilipeBaiaBaia <67919144+FilipeBaiaBaia@users.noreply.github.com>
2023-01-23 17:58:09 +0000

    initial commit, headings and top left logo

---
+++ script.js
@@ -0,0 +1,7 @@
+
+        function changeColor() {
+        document.querySelector("rect").style.fill = "palegreen";
+        }
+        function returnColor() {
+        document.querySelector("rect").style.fill = "none";
+        }
