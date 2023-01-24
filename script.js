d14aeaf5a6140f88ef0262d0a90116a14f1fcd9b
FilipeBaiaBaia <67919144+FilipeBaiaBaia@users.noreply.github.com>
2023-01-24 18:04:45 +0000

    progress

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
         function changeColor() {
-        document.querySelector("rect").style.fill = "palegreen";
+        document.querySelector("rect").style.fill = "rgba(47,79,79, 0.6)" ;
         }
         function returnColor() {
         document.querySelector("rect").style.fill = "none";
